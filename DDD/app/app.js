eb60d772d40b4d7c5be40ebf1d0a2e3a84ef15b7
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-11 15:52:01 +0200

    Setup morgan middleware

--- DDD/app/app.js
+++ DDD/app/app.js
@@ -1,8 +1,10 @@
 require("./db/db_connection");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 module.exports = (app) => {
   app.use(express.json());
   app.use(cors());
+  app.use(morgan("dev"));
 };
