00c0a7910487d32d612eb259e2d915057a41cf53
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-11 15:37:36 +0200

    edit server port

--- DDD/index.js
+++ DDD/index.js
@@ -1,6 +1,5 @@
-const express = require("express");
-const app = express();
+const server = require("express")();
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+server.listen(PORT, HOST, () => console.log(`Server start at port: ${PORT}`));
