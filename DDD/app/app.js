6e6ddba7c5d3f306d02dc84bdc06736cd0e2d7aa
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-11 15:54:11 +0200

    Setup helmet middleware

--- DDD/app/app.js
+++ DDD/app/app.js
@@ -2,9 +2,10 @@ require("./db/db_connection");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const helmet = require("helmet");
 module.exports = (app) => {
   app.use(express.json());
-  app.use(cors());
   app.use(morgan("dev"));
+  app.use(helmet());
+  app.use(cors());
 };
