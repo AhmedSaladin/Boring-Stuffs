2ce947d488bdd08f864dffb64fc66d20cb0938e6
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-11 15:26:07 +0200

    Adding db connection string

--- DDD/db/db_connection.js
+++ DDD/db/db_connection.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("database", "username", "password", {
-  host: "localhost",
-  dialect: "mysql"
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DILICT,
+  }
+);
+
+sequelize.authenticate().then(() => console.log("Database is connected."));
+sequelize.sync();
+module.exports = sequelize;
