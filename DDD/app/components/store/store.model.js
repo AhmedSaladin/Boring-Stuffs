c75d9bf5846caf1e9dab5fed57f925b314523b3a
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-13 21:38:59 +0200

    Adding store model

---
+++ DDD/app/components/store/store.model.js
@@ -0,0 +1,8 @@
+exports.Store = class Store {
+  constructor(id, storeName, code, address) {
+    this.storeID = id;
+    this.storeName = storeName;
+    this.storeCode = code;
+    this.address = address;
+  }
+};
