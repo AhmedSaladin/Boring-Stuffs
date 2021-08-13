30e613b943fca76105882644a8f6aaac881f7511
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-13 21:40:14 +0200

    Adding isbn

--- DDD/app/components/book/book.model.js
+++ DDD/app/components/book/book.model.js
@@ -1,9 +1,10 @@
 exports.Book = class Book {
-  constructor(id, title, desc, publisher, author, pages) {
+  constructor(id, title, desc, publisher, isbn, author, pages) {
     this.bookID = id;
     this.title = title;
     this.description = desc;
     this.publisher = publisher;
+    this.isbn = isbn;
     this.author = author;
     this.pages = pages;
   }
