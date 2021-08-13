01b6bd3aaf29f19f925aa67fc26e206184da3962
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-13 23:07:01 +0200

    Adding store routes

--- DDD/app/app.js
+++ DDD/app/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const Store = require("./components/store/store.route");
 module.exports = (app) => {
   app.use(express.json());
   app.use(morgan("dev"));
   app.use(helmet());
   app.use(cors());
+  app.use("/", Store);
 };
