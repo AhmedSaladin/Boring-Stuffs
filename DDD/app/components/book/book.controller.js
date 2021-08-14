08bd8db478e646c55f0f636d1856c419b26a7bd9
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-14 22:43:47 +0200

    Adding add new book function

--- DDD/app/components/book/book.controller.js
+++ DDD/app/components/book/book.controller.js
@@ -1,4 +1,5 @@
 const BookDB = require("./book.mysql.db");
+const Book = require("./book.model");
 
 const bookDB = new BookDB();
 
@@ -6,5 +7,11 @@ module.exports = {
   get_books_list: async (req, res) => {
     const books = await bookDB.get_books_list();
     res.status(200).json(books);
+  },
+  add_new_book: async (req, res) => {
+    const { body } = req;
+    const book = await Book(body);
+    await bookDB.add_new_book(book, "a12");
+    res.status(201).json("created");
   },
 };
