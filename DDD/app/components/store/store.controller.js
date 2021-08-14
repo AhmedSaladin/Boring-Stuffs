3e56d6d02f42fca785354d77df76af322e0d3a30
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-14 19:03:41 +0200

    Delete try,catch blocks

--- DDD/app/components/store/store.controller.js
+++ DDD/app/components/store/store.controller.js
@@ -5,22 +5,14 @@ const storeDB = new StoreDB();
 
 module.exports = {
   get_stores_list: async (req, res) => {
-    try {
-      const data = await storeDB.get_stores_list();
-      res.status(200).json(data);
-    } catch (err) {
-      res.status(500).json(err.toString());
-    }
+    const data = await storeDB.get_stores_list();
+    res.status(200).json(data);
   },
 
   add_new_store: async (req, res) => {
-    try {
-      const { name, store_code, address } = req.body;
-      const store = new Store(name, store_code, address);
-      await storeDB.add_new_store(store);
-      res.status(200).json("created");
-    } catch (err) {
-      res.status(500).json(err.toString());
-    }
+    const { name, store_code, address } = req.body;
+    const store = new Store(name, store_code, address);
+    await storeDB.add_new_store(store);
+    res.status(200).json("created");
   },
 };
