55f3d9fd5b1af307cde71c671f3826bf8b5aa1e9
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-14 06:30:47 +0200

    Adding create store route

--- DDD/app/components/store/store.route.js
+++ DDD/app/components/store/store.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { get_store_list } = require("./store.controller");
+const { get_store_list, create_new_store } = require("./store.controller");
 
-router.get("/stores", get_store_list);
+router.get("/stores", get_store_list).post("/stores", create_new_store);
 
 module.exports = router;
