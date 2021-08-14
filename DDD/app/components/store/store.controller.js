bd376fdcbafcc0b52f28910cbde161118f7e02a2
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-14 06:29:36 +0200

    Adding create store function

--- DDD/app/components/store/store.controller.js
+++ DDD/app/components/store/store.controller.js
@@ -1,10 +1,24 @@
 const sequelize = require("./../../db/db_connection");
-const { GET_STORE_LIST } = require("./store.query.mysql");
+const { GET_STORE_LIST, CREATE_NEW_STORE } = require("./store.query.mysql");
+const Store = require("./store.model");
 module.exports = {
   get_store_list: async (req, res) => {
     try {
       const data = await sequelize.query(GET_STORE_LIST, {
         type: sequelize.QueryTypes.SELECT,
+      });
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(500).json(err.toString());
+    }
+  },
+  create_new_store: async (req, res) => {
+    try {
+      const { name, st_code, address } = req.body;
+      const store = new Store(name, st_code, address);
+      const data = await sequelize.query(CREATE_NEW_STORE, {
+        type: sequelize.QueryTypes.INSERT,
+        values: store,
       });
       res.status(200).json(data);
     } catch (err) {
