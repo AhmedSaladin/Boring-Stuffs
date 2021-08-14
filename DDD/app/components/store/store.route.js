a73857d70533d4f2b27b90b1baf5cfa2e87b27ec
Ahmed Saladin <as.yuri74@gmail.com>
2021-08-14 19:05:59 +0200

    Adding try,catch wrapper

--- DDD/app/components/store/store.route.js
+++ DDD/app/components/store/store.route.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const wrapper = require("../../middlewares/async.handler");
 const { get_stores_list, add_new_store } = require("./store.controller");
 
-router.get("/stores", get_stores_list).post("/stores", add_new_store);
+router
+  .get("/stores", wrapper(get_stores_list))
+  .post("/stores", wrapper(add_new_store));
 
 module.exports = router;
